a152eea48fd54e6353a32c885c613b9fe0caddac
robbridges <robbridges@users.noreply.github.com>
2021-03-08 12:34:56 -0800

    updates to project

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const {sequelize, Books} = require('./models');
 
 var app = express();
 
@@ -21,21 +22,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// setting up the database
+(async ()=> {
+  await sequelize.sync();
+  try {
+    await sequelize.authenticate();
+    console.log('You are connected to the database.');
+  } catch (error) {
+    console.error('Uht oh, we could not connect:', error);
+  }
+
+})();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error("We couldn't find the page that you are looking for.");
+  err.status = 404;
+  next(err);
 });
 
-// error handler
+/*
+Middleware to handle errors 
+*/
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.locals.error = err;
+  res.status(err.status);
+  console.error(`There was an ${err.status} error`);
+  console.error(err.stack);
+  if (err.status = 404) {
+    res.render('page-not-found', {error: err});
+  } else {
+    res.render('error', {error: err} );
+  }
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const {Book} = require('../models');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect
 });
 
 module.exports = router;
