3313f69ef4d33ef7b0270722e248e26fb31f4cb1
robbridges <robbridges@users.noreply.github.com>
2021-03-09 21:57:39 -0800

    setting up templates, bit more routing

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,29 @@ var express = require('express');
 var router = express.Router();
 const {Book} = require('../models');
 
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect
-});
+router.get('/', asyncHandler(async (req, res) => {
+  res.redirect('/books')
+}));
+
+router.get('/books', asyncHandler(async (req, res) => {
+  const books = await Book.findAll({ order: [["title", "DESC"]]});
+  res.render('index', {books: books, title: "All Books"});
+}));
+
+router.get('/books/new', asyncHandler(async (req, res) => {
+  res.render('new-book', { book: {}, title: "New Article")
+}));
 
 module.exports = router;
